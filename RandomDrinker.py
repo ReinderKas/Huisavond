64ef1879777fd3fda1a026187037ea12df5d935c
ReinderKas <reinderkas1997@gmail.com>
2024-05-22 13:42:36 +0200

    Random drinker

---
+++ RandomDrinker.py
@@ -0,0 +1,32 @@
+import time
+from random import randrange
+
+
+people = [
+    'Sofia',
+    'NJ   ',
+    'Julia',
+    'Ingvar',
+    'Tjeerd',
+    'Reinder',
+]
+
+drinks = [
+    'beer',
+    'wine',
+    'whiskey',
+    'vodka',
+    'beerenburg',
+]
+
+
+sleepTime = 60      # in seconds
+iteration = 0       # counter to keep track
+
+while True:
+    iteration += 1
+    fucktard = people[randrange(6)]
+    drink = drinks[randrange(5)]
+
+    print('{0}:  {1} \thas to drink  {2}!'.format(iteration, fucktard, drink.capitalize()))
+    time.sleep(sleepTime)
